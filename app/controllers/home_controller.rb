f58db563e2422e5b76d582f2ef1e8e6dce3ec123
Chris Jabas <chris@talismangraphics.com>
2018-09-04 10:24:50 -0500

    Add Mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    ExampleMailer.sample_email(@user).deliver
+    @mailme = ExampleMailer.sample_email(@user).deliver
   end
   =begin
   def create
