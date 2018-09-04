e3a43393e7c1e9d4aff6b478535d616116ae2aba
Chris Jabas <chris@talismangraphics.com>
2018-09-04 10:19:54 -0500

    Add Mail

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,9 @@ module ShopifyApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    gmail_username: 'cjabas@gmail.com'
+    gmail_password: 'Lovenut23!'
+    
   end
 end
-
-gmail_username: 'cjabas@gmail.com'
-gmail_password: 'Lovenut23!'
