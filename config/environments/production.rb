ae6b3dcc0f78bd9950cfdc14a58ac3dff53f24cf
Chris Jabas <chris@talismangraphics.com>
2018-09-04 10:32:59 -0500

    Add Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,6 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
