92877adab1f5395dedbc4fe52667e8a7c676568c
Chris Jabas <chris@talismangraphics.com>
2018-09-06 14:53:36 -0500

    Test 0007

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ShopifyApp::AuthenticatedController
-  require 'feedfunctions'
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
