dda6191112062434dadc7b3743525547e6acf236
Chris Jabas <chris@talismangraphics.com>
2018-09-06 10:59:32 -0500

    Adding Program

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,9 @@
 class ApplicationJob < ActiveJob::Base
   def checkVendor (vendor)
 	  knownVendors = Array["Von Duprin","Cal Royal","Stanley Best"]
-	  
+	    puts "#{knownVendors}"
+  end
+  def createGoogleSpreadsheet()
+	 File.new("testfile.txt") 
   end
 end
